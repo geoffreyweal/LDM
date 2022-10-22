44257a59b6c597c2b8ab477acb44d51e06eebbda
Geoffrey Weal <geoffreyweal@Geoffreys-MBP.staff.vuw.ac.nz>
2022-10-22 19:49:29 +1300

    update version of LDM v0.1

--- Programs/search_internet.py
+++ Programs/search_internet.py
@@ -52,7 +52,8 @@ def get_data_about_google_scholar_search(URL_ori):
 		else:	
 			search_results_split = search_results.split("result")[0]
 
-		print('search_results_split: '+str(search_results))
+		print('search_results: '+str(search_results))
+		print('search_results_split: '+str(search_results_split))
 
 	except AttributeError:
 
