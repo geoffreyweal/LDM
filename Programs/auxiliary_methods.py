af321fc01d83ad9e59a2be30c6911342c90ab740
Geoffrey Weal <geoffreyweal@Geoffreys-MBP.staff.vuw.ac.nz>
2022-10-22 20:40:50 +1300

    update version of LDM v0.1

--- Programs/auxiliary_methods.py
+++ Programs/auxiliary_methods.py
@@ -4,16 +4,16 @@ auxiliary_methods.py, 22/10/2022, Geoffrey R. Weal
 This programs contain general methods that are use across the LDM program
 """
 
-import random
-from time import sleep
-from tqdm import trange
+from random import randrange
+from time   import sleep
+from tqdm   import trange
 
 def wait():
 	"""
 	This method is designed to allow the program to wait for a few seconds.
 	"""
 	#print("Waiting for a few secs...")
-	sleep(random.randrange(1, 6))
+	sleep(randrange(1, 6))
 	#print("Waiting done. Continuing...\n")
 
 def timer(time_to_wait):
